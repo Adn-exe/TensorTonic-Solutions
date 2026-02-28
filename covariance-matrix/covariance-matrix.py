870ab4df6ebde670c3c7928d8a528ff01bfa740f
Adn-exe <adnancarte@gmail.com>
2026-02-28 23:49:11 +0530

    Update solution for covariance-matrix

--- covariance-matrix/covariance-matrix.py
+++ covariance-matrix/covariance-matrix.py
@@ -1,18 +1,17 @@
 import numpy as np
 
 def covariance_matrix(X):
-    X = np.asarray(X, dtype = 'float')
-    N = X.shape[0]
-    
-    if X.ndim != 2 :
-        return None
+  X = np.asarray(X, dtype = 'float')
+  N = X.shape[0]
 
-    if N < 2:
-      return None
-    
-    X_mean = np.mean(X, axis = 0)
-    X_centered = X - X_mean
-    T_Xcentered = X_centered.T
+  if N < 2 or X.ndim != 2:
+    return None
 
-    covariance = (1/(N-1))*(np.dot(T_Xcentered, X_centered))
-    return covariance
+  sample_mean = np.mean(X, axis = 0)
+  X_centered = X - sample_mean
+  Transpose_Xcentered = X_centered.T
+
+  mat_mul = np.dot(Transpose_Xcentered, X_centered)
+  Cov_matrix = (1/(N-1))*mat_mul
+
+  return Cov_matrix
